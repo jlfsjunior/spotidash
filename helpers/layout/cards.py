be83aac1dd892a53c26a6cbd9e3a7d18bfa1fe40
José Luiz Ferreira <jlfsjunior@gmail.com>
2021-08-20 11:50:14 +0200

    Add  artist card

---
+++ helpers/layout/cards.py
@@ -0,0 +1,35 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+def make_artist_card(item):
+
+    if "artists" in item:
+        txt = "Artist: " + item["artists"][0]["name"]
+    else:
+        txt = ""
+
+    return dbc.Card(
+        [
+            dbc.CardImg(src=item["image"], top=True),
+            dbc.CardBody(
+                [
+                    html.H4(item["name"], className="card-title"),
+                    html.A(
+                        item["name"],
+                        href=item["url"],
+                        target="_blank",
+                    ),
+                    html.P(
+                        [
+                            f"Popularity: {item['popularity']}",
+                            txt,
+                        ],
+                        className="card-text",
+                    ),
+                ]
+            ),
+        ],
+        color="dark",
+        inverse=True,
+        style={"width": "18rem"},
+    )
