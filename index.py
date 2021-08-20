0d16e6ab2a9101d864fc0bb0e5550ac14a4c5908
José Luiz Ferreira <jlfsjunior@gmail.com>
2021-08-20 07:17:20 +0200

    Refactoring app to be multi-page

---
+++ index.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+from app import app, server
+
+import plotly.express as px
+
+from pages.index import index_page
+from pages.not_found import not_found_page
+from pages.search import search_page
+from pages.insights import insights_page
+
+app.layout = html.Div([
+    dcc.Location(id="url", refresh=False),
+    html.Div(
+        id="page-content",
+        style={
+            'margin': '25px',
+        },
+    ),
+])
+
+app.validation_layout = html.Div([
+    index_page,
+    search_page,
+    not_found_page,
+])
+
+
+@app.callback(Output("page-content", "children"), Input("url", "pathname"))
+def display_page(pathname):
+
+    if pathname == "/":
+        return index_page
+    elif pathname == "/search":
+        return search_page
+    elif pathname == "/insights":
+        return insights_page
+    else:
+        return not_found_page
+
+
+if __name__ == "__main__":
+    app.run_server(debug=False)
