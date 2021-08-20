33d2660e0ad45716a43f589a4fa612823fa79d9e
José Luiz Ferreira <jlfsjunior@gmail.com>
2021-08-20 14:01:09 +0200

    Add album card

--- helpers/layout/cards.py
+++ helpers/layout/cards.py
@@ -33,3 +33,36 @@ def make_artist_card(item):
         inverse=True,
         style={"width": "18rem"},
     )
+
+
+def make_album_card(item):
+
+    if "artists" in item:
+        txt = "Artist: " + item["artists"][0]["name"]
+    else:
+        txt = ""
+
+    return dbc.Card(
+        [
+            dbc.CardImg(src=item['images'][0]['url'], top=True),
+            dbc.CardBody(
+                [
+                    html.H4(item["name"], className="card-title"),
+                    html.A(
+                        item["name"],
+                        href=item["external_urls"]["spotify"],
+                        target="_blank",
+                    ),
+                    html.P(
+                        [
+                            txt
+                        ],
+                        className="card-text",
+                    ),
+                ]
+            ),
+        ],
+        color="dark",
+        inverse=True,
+        style={"width": "18rem"},
+    )
