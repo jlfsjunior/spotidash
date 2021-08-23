9ee04789a6eeca6fa97aabd733177ff54fbfa257
José Luiz Ferreira <jlfsjunior@gmail.com>
2021-08-23 12:23:14 +0200

    Update tests

--- tests/helpers/test_search.py
+++ tests/helpers/test_search.py
@@ -1,10 +1,16 @@
 from helpers.search import get_search
 
-get_search(search_term, type_str="artist")
-
 def test_helpers001_get_search():
 
     s = get_search("the beatles", type_str="artist")
 
     assert type(s) == "list", "Search did not return a list"
     assert len(s) > 0, "Search returned an empty list"
+
+def test_helpers002_get_search_item():
+
+    s = get_search("the beatles", type_str="artist")
+    s0 = s[0]
+
+    assert type(s0) == "dict", "Search first item did not return a dict"
+    assert "name" in s0.keys(), "Search first item does not have 'name' key"
